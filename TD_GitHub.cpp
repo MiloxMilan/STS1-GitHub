65204855b660987dca952ecf75f7fc129497c38c
MIloxontop <135596501+MiloxMilan@users.noreply.github.com>
2026-03-27 22:12:25 +0100

    EE
    EEE

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -57,6 +57,32 @@ int plusGrand(int a, int b, int c)
 
 
 
+bool estPair(int nb) {
+    if (nb % 2 == 0 || nb % 7 == 0)
+        return true;
+    return false;
+}
+
+int sommeNombre(int nb) {
+    int s = 0;
+    for (int i = 1; i <= nb; i++)
+        s += i;
+    s += nb;
+    return s;
+}
+
+float salaireNet(int salaire) {
+    float net = salaire * 0.77;
+    return net + net * 0.12;
+}
+
+int plusPetit(int a, int b, int c) {
+    int m = a;
+    if (b < m) m = b;
+    if (c < m) m = c;
+    return m;
+}
+
 
 
 
