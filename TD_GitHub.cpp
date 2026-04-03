53bab824d1d61d781ae0d689fd5b723298265d6c
mathislachand-glitch <mathislachand@gmail.com>
2026-04-03 09:35:26 +0200

    Refactor functions and add new features
    ajout des fonctions demander et modification faite

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -14,6 +14,7 @@ bool estMajeur(int age)
 
 int exposant(int nbre, int exp)
 {
+    if (exp > 99) return -1;
     int resultat = 1;
     int compteur = 0;
     while (compteur < exp)
@@ -40,6 +41,12 @@ float TVA(int prix)
 
 int plusGrand(int a, int b, int c)
 {
+    if (a == b || b == c || a == c) {
+        int petit = a;
+        if (b < petit) petit = b;
+        if (c < petit) petit = c;
+        return petit;
+    }
     int grand = a;
     if (b > grand)
     {
@@ -71,9 +78,10 @@ int sommeNombre(int nb) {
     return s;
 }
 
-float salaireNet(int salaire) {
-    float net = salaire * 0.77;
-    return net + net * 0.12;
+float salaireNet(int salaireBrut, float chargeSalariale, int prime) 
+{
+    float net = salaireBrut * (1.0f - (chargeSalariale / 100.0f));
+    return net + prime;
 }
 
 int plusPetit(int a, int b, int c) {
@@ -91,3 +99,36 @@ int plusPetit(int a, int b, int c) {
 
 
 //Fonctions réalisées par l'étudiant 3 :
+
+bool estImpair(int nombre) {
+    // Impair (modulo 2 différent de 0) OU divisible par 8
+    if (nombre % 2 != 0 || nombre % 8 == 0) {
+        return true;
+    }
+    return false;
+}
+
+float moyenne(float a, float b, float c) {
+    float res = (a + b + c) / 3.0f;
+    // Modifié par Étudiant 2 : Retourne -1 si négatif
+    if (res < 0) return -1.0f;
+    return res;
+}
+
+float reduction(float prix, float pourcentage, float tvaValeur) {
+    // Modifié par Étudiant 1 : La TVA est passée en paramètre
+    float prixRemise = prix * (1.0f - (pourcentage / 100.0f));
+    return prixRemise * (1.0f + (tvaValeur / 100.0f));
+}
+
+int nombreMedian(int a, int b, int c) {
+    // Modifié par Étudiant 2 : 999 si au moins deux égaux
+    if (a == b || b == c || a == c) {
+        return 999;
+    }
+
+    // Calcul du médian
+    if ((a <= b && b <= c) || (c <= b && b <= a)) return b;
+    if ((b <= a && a <= c) || (c <= a && a <= b)) return a;
+    return c;
+}
